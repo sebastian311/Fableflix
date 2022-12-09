92ceee8de149bb696417bdc9096501422491e020
NaeSebastian <naesebastian96@gmail.com>
2022-12-09 23:49:47 +0200

    Faq

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,5 +17,16 @@ export class AppComponent {
         {label:'Romanian', value: "Romanian"},
         {label:'English', value: "English"}
     ];
-}
+  }
+
+  toggle(id) {
+    // document.getElementsByClassName("faq-one")[0].childNodes
+    // document.getElementsByClassName("faq-one")[0].childNodes[2].classList.add("inactive") and remove
+    const faq = document.getElementsByClassName(id)[0];
+    (faq.childNodes[2] as any).classList.toggle("active");
+    (faq.childNodes[1] as any).classList.toggle("inactive");
+    // document.getElementsByClassName("faq-one")[0].childNodes[2].classList.toggle("active")
+    
+
+  }
 }
